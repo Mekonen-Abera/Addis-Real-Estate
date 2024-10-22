2fea993890039c8c9e2bc1570e8f7e763974c22a
Mekonen-Abera <makoninabera@gmail,com>
2024-10-22 15:21:19 +0300

    Connect to MongodB

--- API/index.js
+++ API/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
-const app= express();
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log('connected to mongodB!');
+}).catch((err)=>{
+    console.log(err)
+});
+
+const app=express();
 
 app.listen(3000, () =>{
     console.log('server is running on port 3000');
