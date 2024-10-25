812137b0b54314e29b537719bda03c96899133de
Mekonen-Abera <makoninabera@gmail,com>
2024-10-25 11:26:04 +0300

    Create api route

--- client/src/Container/Header.jsx
+++ client/src/Container/Header.jsx
@@ -2,12 +2,12 @@ import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 export default function Header() {
     return (
-    <header className='bg-green-500 shadow-md'>
+    <header className='bg-green-400 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <h1 className='text-bold text-sm sm:text-xl flex-wrap'>
             <span className='text-slate-1000'>Addis </span>
             <span className='text-slate-1000'>Real </span>
-            <span className='text-slate-1000'>State</span>
+            <span className='text-slate-1000'>Estate</span>
         </h1>
         <form className='bg-slate-200 p-3 rounded-e-lg flex items-center'>
             <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
