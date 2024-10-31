e82ae1686baaa2ff993c6f6978296abdb3699074
Mekonen-Abera <makoninabera@gmail,com>
2024-10-31 11:58:12 +0300

    Build Addis Real Estate Website

--- API/index.js
+++ API/index.js
@@ -3,32 +3,36 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.Mongo).then(()=>{
-    console.log('connected to mongodB!');
-}).catch((err)=>{
-    console.log(err)
+mongoose.connect(process.env.Mongo).then(() => {
+    console.log('connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
 });
 
-const app=express();
-
+const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
-app.listen(3002, () =>{
-    console.log('server is running on port 3000');
-}
-);
+app.use("/API/user", userRouter);
+app.use("/API/auth", authRouter);
+app.use("/API/listing", listingRouter);
 
-app.use("/API/user",userRouter);
-app.use("/API/auth",authRouter);
-app.use((err,req,res,next)=>{
-    const statusCode = error.statusCode || 400;
-    const message = error.message || 'Internal Server Error';
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 400;
+    const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
+    success: false,
+    statusCode,
+    message,
+});
+});
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+    console.log('server is running on port 3002');
 });
